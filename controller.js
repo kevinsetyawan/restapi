de82a3e04c0593e93a8f64ebb17e4bc46e6809e8
kevin <kevinsetyawan12360@outlook.com>
2020-07-23 10:28:20 +0700

    berhasil menghapus data

--- controller.js
+++ controller.js
@@ -16,3 +16,64 @@ exports.tampilsemuamahasiswa = function(req,res){
         }
     });
 }; 
+
+
+//menampilkan semua data mahasiswa berdasarkan id
+exports.tampilsemuaberdasarkanid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+    function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows,res)
+        }
+    });
+};
+
+//menambahkan data mahasiswa
+exports.tambahmahasiswa = function(req,res){
+    var id_mahasiswa = req.body.id_mahasiswa;
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (id_mahasiswa,nim,nama,jurusan) VALUES (?,?,?,?)',
+    [id_mahasiswa,nim,nama,jurusan], function(error,rows,fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok("Berhasil menambahkan data",res)
+        }
+    });
+};
+
+//mengubah data berdasarkan id
+exports.ubahmahasiswa = function(req,res){
+    var id_mahasiswa = req.body.id_mahasiswa;
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('UPDATE mahasiswa SET nim=?, nama=?, jurusan=? WHERE id_mahasiswa=?',
+    [nim,nama,jurusan,id_mahasiswa], function(error,rows,fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok("Berhasil ubah data",res)
+        }
+    });
+};
+
+//menghapus data berdasarkan id
+exports.hapusmahasiswa = function(req,res){
+    var id = req.body.id_mahasiswa;
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id],
+    function(error,rows,fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok("Berhasil hapus data",res)
+        }
+    });
+}; 
