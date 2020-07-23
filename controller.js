646b0517b5bca8697c080e40f7509f11ef9e9bf2
kevin <kevinsetyawan12360@outlook.com>
2020-07-23 10:24:46 +0700

    berhasil tambah data

--- controller.js
+++ controller.js
@@ -1,8 +1,18 @@
 'use strict';
-
 var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req, res){
     response.ok("Aplikasi REST API ku berjalan!",res)
 };
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows,res)
+        }
+    });
+}; 
